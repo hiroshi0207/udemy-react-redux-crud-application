d708c999ccad717b103ba4cf9cea4dc14b09eac0
hnoguchi0207 <hnoguchi@mb.infoweb.ne.jp>
2020-08-12 23:36:57 +0900

    implement action and action creator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component}  from 'react';
 
-const App = () => {
-  const profiles = [
-  {name: "Taro", age: 10},
-  {name: "Hanako", age: 5},
-  {name: "noname"}
-  ]
 
-  return  <div>
-     {profiles.map((profile, index) => {
-         return <User name={profile.name} age={profile.age} key={index} />
-     })
-    }
-    </div>;
+const App = () => (<Counter> </Counter>)
+
+class Counter extends Component  {
+  constructor(props) {
+    super(props)
+    console.log("this.state")
+    console.log(this.state)
+    this.state = {count: 0}
+
+  }
+
+  handlePlusButton = () => {
+    console.log("hundlePlusButton")
+    console.log(this.state)
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    console.log("hundleMinusButton")
+    console.log(this.state)
+    this.setState({count: this.state.count - 1})
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+      <div>counter: {this.state.count}</div>
+      <button onClick={this.handlePlusButton}> +1 </button>
+      <button onClick={this.handleMinusButton}> -1 </button>
+      </React.Fragment>
+    )
+  }
 }
-
-const User = (props) => {
-   return <div> Hi, I am {props.name}!, and I am {props.age} years old. </div>
-}
-
-User.defaultProps = {
-    age: 1
-}
-
 export default App;
